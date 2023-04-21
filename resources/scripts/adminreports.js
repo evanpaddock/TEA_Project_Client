0dfd15c76a5193ca85f4d17a64d3a1d2c12a3671
arwalker3 <79057893+arwalker3@users.noreply.github.com>
2023-04-21 16:51:08 -0500

    reports

--- resources/scripts/adminreports.js
+++ resources/scripts/adminreports.js
@@ -1,8 +1,10 @@
 function handleOnLoad(){
-    createReport()
+    CreateFirstReport()
+    CreateSecondReport()
+    
 }
 
-function createReport(){
+function CreateFirstReport(){
     const ctx = document.getElementById('myChart').getContext('2d');
     const myChart = new Chart(ctx, {
       type: 'bar',
@@ -17,11 +19,16 @@ function createReport(){
         }]
       },
       options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
+        responsive: true,
+        autoPadding: true,
+        layout: {
+          padding: 20
+      }
+        // scales: {
+        //   // y: {
+        //   //   beginAtZero: true
+        //   // }
+        // }
       }
     });
     // Fetching data from mySQL
@@ -36,4 +43,45 @@ function createReport(){
     .catch(error => {
         console.error('Error fetching sales data', error)
     })
+}
+
+function CreateSecondReport(){
+  const ctx = document.getElementById('secondChart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: [],
+      datasets: [{
+        label: 'Sales',
+        data: [],
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      autoPadding: true,
+      layout: {
+        padding: 20
+    }
+      // scales: {
+      //   // // y: {
+      //   //   beginAtZero: true
+      //   // }
+      // }
+    }
+  });
+  // Fetching data from mySQL
+  fetch('insert link here')
+  .then(response => response.json())
+  .then(data =>{
+      //Updates the chart
+      myChart.data.labels = data.map(sale => sale.date)
+      myChart.data.datasets[0] = data.map(sale.amount)
+      myChart.update()
+  })
+  .catch(error => {
+      console.error('Error fetching sales data', error)
+  })
 }
