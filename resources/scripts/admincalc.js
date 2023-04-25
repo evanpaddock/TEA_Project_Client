3638237ee6b9d66926dde29fff3acb09ef4d9d2d
arwalker3 <arwalker3@users.noreply.github.com>
2023-04-24 23:17:26 -0500

    unfinished admiccalc - why edit wont work properly

--- resources/scripts/admincalc.js
+++ resources/scripts/admincalc.js
@@ -15,17 +15,15 @@ function handleOnLoad(){
 // getData()
 
 const data = [
-    { make: "Ford", model: "Mustang", year: 2019, deleted: false},
-    { make: "Tesla", model: "Model S", year: 2020, deleted: false },
-    { make: "Chevrolet", model: "Corvette", year: 2021, deleted: false },// New vehicle added
+    {id: 0, make: "Ford", model: "Mustang", year: 2019, deleted: false},
+    {id: 1, make: "Tesla", model: "Model S", year: 2020, deleted: false },
+    {id: 2, make: "Chevrolet", model: "Corvette", year: 2021, deleted: false },// New vehicle added
   ];
 const tableHtml = createTable(data);
 document.getElementById("table-container").innerHTML = tableHtml;
 
 
 const addCarBtn = document.getElementById("add-car-btn");
-
-
 addCarBtn.addEventListener("click", () => {
   const make = prompt("Enter car make:");
   const model = prompt("Enter car model:");
@@ -51,76 +49,29 @@ addCarBtn.addEventListener("click", () => {
         </thead>
         <tbody>
     `;
-    data.forEach((item, index) => {
+    data.forEach((item, id) => {
+        if(data[id].deleted != true){
         tableHtml += `<tr class="table-dark">
-        <td>${index + 1}</td>
+        <td>${id + 1}</td>
         <td>${item.make}</td>
         <td>${item.model}</td>
         <td>${item.year}</td>
         <td>
-            <button class="btn btn-secondary edit-car-btn" data-index="${index}">Edit</button>
+            <button id="edit-car-btn" class="btn btn-secondary" data-index="${id}">Edit</button>
           </td>
         <td>
-            <button class="btn btn-danger delete-car-btn" data-index="${index}">Delete</button>
+            <button id="delete-car-btn" class="btn btn-danger" data-index="${id}">Delete</button>
         </td>
       </tr>
     `;
+      }    
     });
+
     tableHtml += `
             </tbody>
         </table>
         `;
   
-    const tableElement = document.createElement("div");
-    tableElement.innerHTML = tableHtml;
   
-    tableElement.querySelectorAll(".edit-car-btn").forEach(btn => {
-      btn.addEventListener("click", () => {
-        const index = parseInt(btn.getAttribute("data-index"));
-        const make = prompt("Enter updated car make:", data[index].make);
-        const model = prompt("Enter updated car model:", data[index].model);
-        const year = parseInt(prompt("Enter updated car year:", data[index].year));
-        data[index].make = make;
-        data[index].model = model;
-        data[index].year = year;
-        const tableHtml = createTable(data);
-        document.getElementById("table-container").innerHTML = tableHtml;
-      });
-    });
-  
-    tableElement.querySelectorAll(".delete-car-btn").forEach(btn => {
-      btn.addEventListener("click", () => {
-        const index = parseInt(btn.getAttribute("data-index"));
-        data[index].deleted = true;
-        const tableHtml = createTable(data);
-        document.getElementById("table-container").innerHTML = tableHtml;
-        console.log(data[index].deleted)
-      });
-    });
-  
-    return tableElement.innerHTML;
+    return tableHtml;
   }
-
-
-document.querySelectorAll(".edit-car-btn").forEach(btn => {
-    btn.addEventListener("click", () => {
-      const index = parseInt(btn.getAttribute("data-index"));
-      const make = prompt("Enter updated car make:", data[index].make);
-      const model = prompt("Enter updated car model:", data[index].model);
-      const year = parseInt(prompt("Enter updated car year:", data[index].year));
-      data[index].make = make;
-      data[index].model = model;
-      data[index].year = year;
-      const tableHtml = createTable(data);
-      document.getElementById("table-container").innerHTML = tableHtml;
-    });
-  });
-
-  document.querySelectorAll(".delete-car-btn").forEach(btn => {
-    btn.addEventListener("click", () => {
-      const index = parseInt(btn.getAttribute("data-index"));
-      data[index].deleted = true;
-      const tableHtml = createTable(data);
-      document.getElementById("table-container").innerHTML = tableHtml;
-    });
-  });
