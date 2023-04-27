456304e717aef275026ef9b520c274bafaaa56d4
arwalker3 <arwalker3@users.noreply.github.com>
2023-04-26 19:33:34 -0500

    working chart 1

--- resources/resources/scripts/adminreports.js
+++ resources/resources/scripts/adminreports.js
@@ -4,31 +4,30 @@ const data = []
 
 function handleOnLoad(){
     CreateFirstReport()
-    CreateSecondReport()
+    // CreateSecondReport()
     
 }
 
 function CreateFirstReport(){
   const getData = function() {
-    fetch(url + "Car")
+    fetch(`${url}AdminReport`)
     .then(function (response){
-        return response.json()
+        return response.json();
     })
     .then(function (data) {
-      //formatting data for use with chart.js
-      const labels = []
-      const values = []
-      data.forEach(function(item){
-        labels.push(item.label)
-        values.push(item.value)
-      })
-    })
+        // Format the data for use with Chart.js
+        const labels = [];
+        const values = [];
+        data.forEach(function(car) {
+            labels.push(car.make);
+            values.push(car.count);
+        });
   
     const ctx = document.getElementById('myChart').getContext('2d');
     const myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: [],
+        labels: labels,
         datasets: [{
           label: 'Most Popular Car by Search',
           data: values,
@@ -50,58 +49,51 @@ function CreateFirstReport(){
         // }
       }
     });
+    data.push(myChart)
     // Fetching data from mySQL, will update when time to connect to API
-    fetch('insert link here')
-    .then(response => response.json())
-    .then(data =>{
-        //Updates the chart
-        myChart.data.labels = data.map(sale => sale.date)
-        myChart.data.datasets[0] = data.map(sale.amount)
-        myChart.update()
-    })
-    .catch(error => {
-        console.error('Error fetching sales data', error)
-    })
+  })
 }
+getData()
+
 }
 
-function CreateSecondReport(){
-  const ctx = document.getElementById('secondChart').getContext('2d');
-  const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: [],
-      datasets: [{
-        label: 'Sales',
-        data: [],
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgba(255, 99, 132, 1)',
-        borderWidth: 1
-      }]
-    },
-    options: {
-      responsive: true,
-      autoPadding: true,
-      layout: {
-        padding: 20
-    }
-      // scales: {
-      //   // // y: {
-      //   //   beginAtZero: true
-      //   // }
-      // }
-    }
-  });
-  // Fetching data from mySQL
-  fetch('insert link here')
-  .then(response => response.json())
-  .then(data =>{
-      //Updates the chart
-      myChart.data.labels = data.map(sale => sale.date)
-      myChart.data.datasets[0] = data.map(sale.amount)
-      myChart.update()
-  })
-  .catch(error => {
-      console.error('Error fetching sales data', error)
-  })
-}
+// function CreateSecondReport(){
+//   const ctx = document.getElementById('secondChart').getContext('2d');
+//   const myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//       labels: [],
+//       datasets: [{
+//         label: 'Sales',
+//         data: [],
+//         backgroundColor: 'rgba(255, 99, 132, 0.2)',
+//         borderColor: 'rgba(255, 99, 132, 1)',
+//         borderWidth: 1
+//       }]
+//     },
+//     options: {
+//       responsive: true,
+//       autoPadding: true,
+//       layout: {
+//         padding: 20
+//     }
+//       // scales: {
+//       //   // // y: {
+//       //   //   beginAtZero: true
+//       //   // }
+//       // }
+//     }
+//   });
+//   // Fetching data from mySQL
+//   fetch(`${url}`)
+//   .then(response => response.json())
+//   .then(data =>{
+//       //Updates the chart
+//       myChart.data.labels = data.map(car => car.make)
+//       myChart.data.datasets[0] = data.map(car.count)
+//       myChart.update()
+//   })
+//   .catch(error => {
+//       console.error('Error fetching sales data', error)
+//   })
+// }
